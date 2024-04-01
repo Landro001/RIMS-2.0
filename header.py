49eb47a2e38cedf455fbce38e3f70f07ef8cd166
Leandro <ljborges76@gmail.com>
2024-04-01 16:47:37 -0300

    Adicionando background e iniciando o header

---
+++ header.py
@@ -0,0 +1,8 @@
+# Header da aplicação
+
+# Modulos
+from flet import *
+from controls import add_to_control_reference, return_control_reference
+
+# Definir o dicionário retornado como variável no topo da classe
+control_map = return_control_reference()
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+# Modulos
+import flet
 from flet import *
 import sqlite3
 
 def main(page:Page):
+    page.bgcolor = "#F7ECE1"
+    page.padding = 20
+    page.add(
+        Column(
+            expand=True,
+            controls=[
+
+            ],
+        )
+    )
+    page.update()
     pass
 
-app(target=main)
+    
+
+
+flet.app(target=main)
